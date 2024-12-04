eef7318fd45b3b31a7b665bb535fa9958f427b3c
supriYadi <supriyadi185@gmail.com>
2024-12-05 04:21:01 +0700

    Add files via upload

---
+++ media2mp3mono.sh
@@ -0,0 +1,24 @@
+#!/system/bin/sh
+folder="$(dirname $(realpath $0))"
+if [ -z "$1" ]
+then
+echo "usage: bash $0 media"
+kill -9 $$
+fi
+##
+ffmpeg -i "$@" 2>&1|grep -i "Audio"|busybox cut -d ' ' -f 1- > $folder/info.log
+cat $folder/info.log|busybox tr ',' '\x0a' > $folder/info2.log
+cat $folder/info2.log|busybox sort -n -r > $folder/info3.log
+cat $folder/info3.log|grep -i "kb/s"|busybox awk '{print $1}' > $folder/kbps
+bitrate="$(cat $folder/kbps)k"
+busybox echo -e "Media info\n\n$(cat $folder/info.log)\n\n"
+echo "Build new script..."
+sleep 4
+echo "#!/system/bin/sh" > $folder/02-$(basename $0)
+echo "ffmpeg -i \"$@\" -ac 1 -ab $bitrate $(dirname $@)/Output-$(date +%s).mp3" >> $folder/02-$(basename $0)
+$(readlink /proc/$$/exe) "$folder/02-$(basename $0)"
+### Author: KambingHitam
+## perintah head hanya 2 string yang di-print, baris pertama dan kedua
+### bagaimana caranya hanya print string yg ke 2/baris kedua??
+### apa alternativ perintah "head"
+### ada yang bisa bantu..?
